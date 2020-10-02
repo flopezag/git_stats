e303242fab27ebb2f121a795145b167f9dbebea4
Fernando Lopez <fernando.lopez@fiware.org>
2020-10-02 09:55:48 +0200

    Added forks analysis of the repo

--- git_stats.go
+++ git_stats.go
@@ -47,6 +47,33 @@ func unique(authors []string) []string {
 	return list
 }
 
+func getForkedRepos(context context.Context, client *github.Client, organization string, repository string) ([]string, []string) {
+	var owner []string // List of owners of the different repositories, including main owner and all forked ones
+	var repo []string  // List of repo name of the different repositories, including main owner and all forked ones
+
+	/*opt := &RepositoryListForksOptions{
+		ListOptions: ListOptions{PerPage: 30},
+	}*/
+
+	owner = append(owner, organization)
+	repo = append(repo, repository)
+
+	// Need to check if there is more than 30 forks in a repo
+	forks, _, err := client.Repositories.ListForks(context, organization, repository, nil)
+
+	if err != nil {
+		fmt.Printf("Problems getting branches information %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, v := range forks {
+		owner = append(owner, v.Owner.GetLogin())
+		repo = append(repo, v.GetName())
+	}
+
+	return owner, repo
+}
+
 func getAuthors(context context.Context, client *github.Client, organization string, repository string) []string {
 	// For each branch, we need to get the unique list of commits authors
 	// List branches	GET	/repos/{owner}/{repo}/branches{?page}	Done	Done
@@ -54,56 +81,69 @@ func getAuthors(context context.Context, client *github.Client, organization str
 	var s []string
 	var aux string
 
+	// Initialize the CommitsLisOptions parameter
 	opt := &github.CommitsListOptions{
 		ListOptions: github.ListOptions{PerPage: 30},
 	}
 
-	branches, _, err := client.Repositories.ListBranches(context, organization, repository, nil)
+	owner, repo := getForkedRepos(context, client, organization, repository)
 
-	if err != nil {
-		fmt.Printf("Problems getting branches information %v\n", err)
-		os.Exit(1)
-	}
+	for i, u := range owner {
+		t := repo[i]
 
-	for _, v := range branches {
-		fmt.Printf("Branch sha: %+v\n", v.Commit.GetSHA())
+		fmt.Printf("\nOwner: %+v      Repo: %+v\n", u, t)
 
-		opt.SHA = v.Commit.GetSHA()
-		opt.Page = 0
+		// First, for the main repo extract all branches and get the owners of all commits
+		// Need to check if there is more than 30 branches in one repo...
+		branches, _, err := client.Repositories.ListBranches(context, u, t, nil)
 
-		for {
-			commitInfo, resp_commits, err := client.Repositories.ListCommits(context, organization, repository, opt)
-
-			if err != nil {
-				fmt.Printf("Problem in commit information %v\n", err)
-				os.Exit(1)
-			}
-
-			fmt.Printf("Page: %d", resp_commits.NextPage)
-
-			// Get the list of all commit authors
-			for _, v := range commitInfo {
-				aux = v.Author.GetLogin()
-
-				if aux == "" {
-					aux = v.Commit.Author.GetName()
-				}
-
-				// fmt.Printf("index %d, value %+v\n", i, aux)
-				s = append(s, aux)
-			}
-
-			fmt.Printf("  Number of commits: %d\n", len(commitInfo))
-
-			if resp_commits.NextPage == 0 {
-				break
-			}
-
-			opt.Page = resp_commits.NextPage
-
-			s = unique(s)
+		if err != nil {
+			fmt.Printf("Problems getting branches information %v\n", err)
+			os.Exit(1)
 		}
 
+		for _, v := range branches {
+			fmt.Printf("Branch sha: %+v\n", v.Commit.GetSHA())
+
+			opt.SHA = v.Commit.GetSHA()
+			opt.Page = 0
+
+			for {
+				commitInfo, resp_commits, err := client.Repositories.ListCommits(context, u, t, opt)
+
+				if err != nil {
+					fmt.Printf("Problem in commit information %v\n", err)
+					os.Exit(1)
+				}
+
+				fmt.Printf("Page: %d", resp_commits.NextPage)
+
+				// Get the list of all commit authors
+				for _, w := range commitInfo {
+					aux = w.Author.GetLogin()
+
+					if aux == "" {
+						aux = w.Commit.Author.GetName()
+					}
+
+					// fmt.Printf("index %d, value %+v\n", i, aux)
+					s = append(s, aux)
+				}
+
+				fmt.Printf("  Number of commits: %d\n", len(commitInfo))
+
+				if resp_commits.NextPage == 0 {
+					break
+				}
+
+				opt.Page = resp_commits.NextPage
+
+				s = unique(s)
+			}
+
+		}
+
+		s = unique(s)
 	}
 
 	var uniqueAuthors = unique(s)
@@ -143,8 +183,9 @@ func main() {
 
 	s = getAuthors(context, client, "flopezag", "github-api-testing")
 
+	fmt.Printf("\nResults obtained (different users)\n")
 	for i, v := range s {
-		fmt.Printf("index %d, value %+v\n", i, v)
+		fmt.Printf("    Index %d, value %+v\n", i, v)
 	}
 
 }
